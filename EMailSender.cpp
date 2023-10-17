a3eea2e8835fdac435a04d853382f8062f768038
Renzo Mischianti <renzo.mischianti@gmail.com>
2023-10-17 14:30:16 +0200

    Fix warnings #43

--- EMailSender.cpp
+++ EMailSender.cpp
@@ -2,7 +2,7 @@
  * EMail Sender Arduino, esp8266, stm32 and esp32 library to send email
  *
  * AUTHOR:  Renzo Mischianti
- * VERSION: 3.0.11
+ * VERSION: 3.0.12
  *
  * https://www.mischianti.org/
  *
